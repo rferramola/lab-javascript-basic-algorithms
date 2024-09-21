610f3a551ffc89723267ac7f3769c99aa0a285f9
Roxana Ferramola <ferramolafvm@gmail.com>
2024-09-21 13:05:29 +0200

    interation-2

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 // Iteration 1: Names and Input
 const hacker1 = "Tomás"
-console.log(hacker1);
+console.log(`The driver's name is ${hacker1}`);
 const hacker2 = "Èric"
-console.log(hacker2);
+console.log(`The navigator's name is ${hacker2}`);
 const hacker3 = "Roxana"
-console.log(hacker3);
-
-
+console.log(`The navigator's name is ${hacker3}`);
 
 
 // Iteration 2: Conditionals
+let longestName 
+
+if (hacker1.length > hacker2.length && hacker1.length > hacker3.length ) {
+    longestName = hacker1
+
+} else if(hacker2.length > hacker1.length && hacker2.length > hacker3.length ){
+    longestName = hacker2
+} else (longestName = hacker3)
+
+if(longestName === hacker1){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+
+} else if(longestName === hacker2){
+    console.log(`It seems that the navigator1 has the longest name, it has ${hacker2.length} characters.`);
+
+} else if(longestName === hacker3){
+    console.log(`It seems that the navigator2 has the longest name, it has ${hacker3.length} characters.`);
+
+} else{console.log(`wow, you all have equally longs names, ${hacker1.length} characters. `)}
+
+
 
 
 // Iteration 3: Loops
